f0f05f01664e80945b85554e199f83e8952d77e1
Carol <carol.tsai0224@gmail.com>
2022-08-10 13:14:17 -0400

    created variablePractice, deleted HelloWorld

---
+++ src/main/java/com/technews/VariablePractice.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariablePractice {
+    public static void main(String[] args) {
+        int three = 3;
+        double thirds = 3.333;
+        double dsum = three + thirds;
+        String hello = "Hello";
+        int four = 4;
+        String strsum = hello + four;
+        System.out.println(dsum);
+        System.out.println(strsum);
+    }
+}
